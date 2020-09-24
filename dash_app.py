ba250339f8b248cc5f70bca01dc7457554ec0674
Steven Lantigua <slantigua12@gmail.com>
2020-09-24 00:13:52 -0400

    Update dash_app.py

--- dash_app.py
+++ dash_app.py
@@ -29,82 +29,116 @@ app_color = {
     "graph_line": "rgb(8, 70, 151)",
     "graph_font":"rgb(2, 29, 65)"}
 
+table_header_style = {
+    "backgroundColor": "rgb(2,21,70)",
+    "color": "white",
+    "textAlign": "center",
+}
+
 #--------------Configuring Dash App---------------------------------#
 app = dash.Dash(__name__)
 
 #------------------------Layout------------------------------#
 app.layout = html.Div(
+	#className='container',
 	children=[
-		# --------------Contents------------------------------------------------#
-		html.Div(className='row',
-			children=[
-			# -------------------left side of app--------------------#
-			html.Div(className='seven columns div-user-controls',
+			# header
+
+			html.Div(
+				className="app__header row",
 				children=[
-				
-				html.H2('Amazon Analysis'),
-				html.P('''A deep dive into what twitter users are saying about Amazon in real time.'''),
-				#-------------graph-----------------#
-				html.Div([
-					dcc.Graph(
-						id='live-graph',
-						animate=False,
-						figure=go.Figure(
-							layout=go.Layout(
-								plot_bgcolor=app_color["graph_bg"],
-								paper_bgcolor=app_color["graph_bg"]))
-					),
-					dcc.Interval(
-						id='graph-update',
-						interval=1*10000,  # in milliseconds
-						n_intervals=0,
-						)
-					]),
-				# ----------------Live Twitter Feed----------------#
-				html.Div([
-					html.H4('Live Twitter Feed'),
-					html.Div(id='update_text'),
-					dcc.Interval(
-						id='feed-update',
-						interval=1*10000,
-						n_intervals=0,
-						)
-					]
-				),
-			]),
-			#-----------------------Right side: bar plot and pie chart-------------------#
-			html.Div(className='five columns div-user-controls',
-				children=[
-			# -------------------------pie chart----------------------#
-				html.Div([
-					html.H6('Sentiment by the Percentages'),
-					dcc.Interval(
-						id='pie_update',
-						interval=1*10000,
-						n_intervals=0),
-					dcc.Graph(id='pie_chart'),
-					]),
-				# ------------------------word count bar chart---------------#
-				html.Div([
-					dcc.Interval(
-						id='bar-update',
-						interval=1*10000,
-						n_intervals=0,
-						),
-					dcc.Graph(
-						id='word_count',
-						animate=False,
-						figure=go.Figure(
-							layout=go.Layout(
-								plot_bgcolor=app_color["graph_bg"],
-								paper_bgcolor=app_color["graph_bg"],
-								)
-							)
-						)
-					]),
-				])
-			])
-		])
+					html.A(
+			                className="div-logo",
+			                children=[
+			                	html.Img(
+			                    	className="logo", src=app.get_asset_url("dash-logo-new.png")
+			                	)
+			                ]
+			            ),
+				#title
+					html.Div(
+						className='study-browser-banner',
+						children=[
+						html.H2('Analyzing Amazon'),
+						html.P('''Taking a deeper look into what Twitter users are saying about Amazon in real time.''')
+						]	
+					)
+            	]
+        	),
+            html.Div(
+            	className='container',
+            	children=[
+            		html.Div(
+            			
+            			children=[
+
+				            # Left side
+				            html.Div(
+				            	className='seven columns ',
+				            	children=[
+								#-------------graph-----------------#
+									html.Div([
+										dcc.Graph(
+											id='live-graph',
+											animate=False,
+											
+										),
+										dcc.Interval(
+											id='graph-update',
+											interval=1*10000,  # in milliseconds
+											n_intervals=0,
+											)
+										]
+									),
+								# ----------------Live Twitter Feed----------------#
+									html.Div([
+										html.H6('Live Twitter Feed'),
+										html.Div(id='update_text'),
+										dcc.Interval(
+											id='feed-update',
+											interval=1*10000,
+											n_intervals=0,
+											)
+										]
+									),
+								]
+							),
+							#-----------------------Right side: bar plot and pie chart-------------------#
+							html.Div(
+								className='five columns',
+								children=[
+								# -------------------------pie chart----------------------#
+									html.Div([
+										#html.H6('Sentiment by the Percentages'),
+										dcc.Interval(
+											id='pie_update',
+											interval=1*10000,
+											n_intervals=0),
+										dcc.Graph(id='pie_chart'
+											),
+										]
+									),
+									# ------------------------word count bar chart---------------#
+									html.Div([
+										dcc.Interval(
+											id='bar-update',
+											interval=1*10000,
+											n_intervals=0,
+											),
+										dcc.Graph(
+											id='word_count',
+											animate=False,
+											),
+										]
+									),
+								]
+							),
+						],
+					)
+        		],		
+			)
+		]	
+	)
 
 @app.callback(Output('live-graph', 'figure'),
 	[Input('graph-update', 'n_intervals')])
@@ -160,17 +194,18 @@ def update_pie(interval):
 		labels = ['Positive', 'Negative', 'Neutral']
 		vals_sent = [vals['Pos'], vals['Ng'], vals['Nu']]
 
-		data = go.Pie(labels=labels, values=vals_sent, hole=.5, pull=[0,0,0])
+		data = go.Pie(labels=labels, values=vals_sent, hole=.3, pull=[0,0,0])
 		layout = go.Layout(
+						title='Sentiment by the Percentages'.format(),
 						autosize=True,
 						margin=go.layout.Margin(
 							l=75,
-							r=25,
+							r=30,
 							b=70,
-							t=25,
+							t=40,
 							pad=4),
 						plot_bgcolor=app_color["graph_bg"],
-						paper_bgcolor='rgba(0,0,0,0)')
+						paper_bgcolor=app_color["graph_bg"])
 
 		return go.Figure(data=[data], layout=layout)
 			
@@ -199,38 +234,48 @@ def bag_of_words(series):
 
 
 def update_graph_bar(interval):
-	query = "SELECT * FROM AmazonTweets"
-	df = fetchTweets(query)
-	# get the counter for all the tokens
-	word_counter = bag_of_words(df.tweet)
+	try:
+		query = "SELECT * FROM AmazonTweets"
+		df = fetchTweets(query)
+		# get the counter for all the tokens
+		word_counter = bag_of_words(df.tweet)
 
-	# get the most common n tokens
-	# n is specified by the slider
-	top_n = word_counter.most_common(10)[::-1]
+		# get the most common n tokens
+		# n is specified by the slider
+		top_n = word_counter.most_common(10)[::-1]
 
-	# get the x and y values
-	X = [cnt for word, cnt in top_n]
-	Y = [word for word, cnt in top_n]
+		# get the x and y values
+		X = [cnt for word, cnt in top_n]
+		Y = [word for word, cnt in top_n]
 
-	 # plot the bar chart
-	bar_chart = go.Bar(
-		x=X, y=Y,
-		name='Word Counts',
-		orientation='h',
-		)
+		 # plot the bar chart
+		bar_chart = go.Bar(
+			x=X, y=Y,
+			name='Word Counts',
+			orientation='h',
+			)
 
-	layout = go.Layout(
-		title='Word Count',
-		height=250,
-		width=400,
-		autosize=True,
-		plot_bgcolor=app_color["graph_bg"],
-		paper_bgcolor=app_color['graph_bg'],
-		margin=go.layout.Margin(
-			l=20, r=20, b=30, t=30, pad=4),
-		xaxis=dict(title_text='Frequency'))
+		layout = go.Layout(
+			title='Word Count',
+			height=250,
+			width=400,
+			autosize=True,
+			plot_bgcolor=app_color["graph_bg"],
+			paper_bgcolor=app_color['graph_bg'],
+			margin=go.layout.Margin(
+				l=75, 
+				r=25, 
+				b=30, 
+				t=30, 
+				pad=4),
+			xaxis=dict(title_text='Frequency'))
 
-	return go.Figure(data=bar_chart, layout=layout)
+		return go.Figure(data=bar_chart, layout=layout)
+
+	except Exception as e:
+			with open('errors.txt', 'a') as f:
+				f.write(str(e))
+				f.write('\n')
 
 
 #----------------update twitter feed-------------------------#
@@ -238,21 +283,20 @@ def update_graph_bar(interval):
 			 [Input('feed-update', 'n_intervals')])
 
 def update_tweet(tweets):
+	try:
+		tweetquery = "SELECT * FROM AmazonTweets WHERE tweet NOT LIKE 'RT %' ORDER BY unix DESC"
+		tweetdf = fetchTweets(tweetquery)
 
-	tweetquery = "SELECT * FROM AmazonTweets WHERE tweet NOT LIKE 'RT %' ORDER BY unix DESC"
-	tweetdf = fetchTweets(tweetquery)
-
-	'''if len(tweetdf) > 500:
-					tweetdf.c.excecute("DELETE FROM AmazonTweets")
-					tweetdf.conn.commit()'''
-
-	return html.Div([
-		html.P(tweetdf['tweet'][0]),
-		html.P(tweetdf['tweet'][1]),
-		html.P(tweetdf['tweet'][2])
-        ])
-
-
+		return html.Div([
+			html.P(tweetdf['tweet'][0]),
+			html.P(tweetdf['tweet'][1]),
+			html.P(tweetdf['tweet'][2])
+	        ])
+	
+	except Exception as e:
+		with open('errors.txt', 'a') as f:
+			f.write(str(e))
+			f.write('\n')
 
 # run the app
 if __name__ == '__main__':
